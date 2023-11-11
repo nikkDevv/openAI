d59b8e45072063cab0d8938f8bbf4fb8de0c227f
nikk <nickviduya.dev@gmail.com>
2023-11-12 06:43:16 +0800

    tolstoy GPT full integration

--- openAIconfig.js
+++ openAIconfig.js
@@ -4,28 +4,58 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000; // You can choose any port
+const port = 3000;
 
-const openai = new OpenAI({ apiKey: process.env.API_KEY });
+const openai = new OpenAI({
+  apiKey: process.env.API_KEY
+});
+
+const assistantId = 'asst_pqAvzW9n3XHlsLEl9dkxrzfV';
 let conversationHistory = [];
 
-// Function to interact with OpenAI
 async function askGPT(question) {
     try {
-        conversationHistory.push({ "role": "user", "content": question });
+        conversationHistory.push({"role": "user", "content": question});
+        // Retrieve the Assistant
+        const myAssistant = await openai.beta.assistants.retrieve(assistantId);
+        console.log(myAssistant); // For debugging
 
-        const chatCompletion = await openai.chat.completions.create({
-            model: "gpt-4-1106-preview", 
-            messages: conversationHistory
+        // Create a new thread for each conversation
+        const threadResponse = await openai.beta.threads.create();
+
+        // Add a message to the thread with the user's question
+        await openai.beta.threads.messages.create(threadResponse.id ,{
+            role: "user",
+            content: question
         });
 
-        const aiResponse = chatCompletion.choices[0].message.content;
-        conversationHistory.push({ "role": "system", "content": aiResponse });
+        // Run the assistant to get a response
+        const run = await openai.beta.threads.runs.create(
+            threadResponse.id,
+            { assistant_id:  assistantId }
+          );
+        
+        let runStatus = await openai.beta.threads.runs.retrieve(
+            threadResponse.id,
+            run.id
+          );
 
-        return aiResponse;
+        // Polling for run completion
+        while (runStatus.status !== 'completed') {
+            await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
+            runStatus = await openai.beta.threads.runs.retrieve(threadResponse.id, run.id);
+        }
+
+        // Retrieve the messages after the assistant run is complete
+        const messagesResponse = await openai.beta.threads.messages.list(threadResponse.id);
+        
+        const aiMessages = messagesResponse.data.filter(msg => msg.role === 'assistant');
+
+        // Assuming the last message is the assistant's response
+        return aiMessages[aiMessages.length - 1].content[0].text.value;
     } catch (error) {
-        console.error('Error in askGPT:', error);
-        return null;
+        console.error('Error in askGPT:', error.response ? error.response.data : error);
+        return 'An error occurred while processing your request.'; // Placeholder response
     }
 }
 
